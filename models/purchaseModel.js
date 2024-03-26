2319136e745135b2376c86ff1cd7b7a7509268e9
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-03-27 01:35:55 +0300

    Purchase model is added.

---
+++ models/purchaseModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const purchaseSchema = mongoose.Schema({
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "A purchase must have a seller"],
+  },
+  buyer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "A purchase must have a buyer"],
+  },
+  beat: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Beat",
+    required: [true, "A purchase must have a beat"],
+  },
+  date: {
+    type: Date,
+    required: [true, "A purchase must have a date"],
+    default: () => new Date.now(),
+  },
+  license: {
+    type: String,
+    required: [true, "A purchase must have a license"],
+  },
+});
+
+const Purchase = mongoose.Model("Purchase", purchaseSchema);
+
+module.exports = Purchase;
