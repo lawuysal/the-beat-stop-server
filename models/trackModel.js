6ffb761c192c5885e1b8fe83c594e235f1ed6aac
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-03-26 23:12:14 +0300

    Track model added.

---
+++ models/trackModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const trackSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A track must have a name"],
+  },
+  uploadedAt: {
+    type: Date,
+    default: () => new Date.now(),
+  },
+  duration: {
+    type: String,
+    required: [true, "A track must have a duration"],
+  },
+  fileType: {
+    type: String,
+    required: [true, "A track must have a file type"],
+  },
+  link: {
+    type: String,
+    required: [true, "A track must have a link"],
+  },
+  uploader: {
+    type: mongoose.Schema.ObjectId,
+    ref: User,
+    required: [true, "A track must have an uploader"],
+  },
+});
+
+const Track = mongoose.Model("Track", trackSchema);
+
+module.exports = Track;
