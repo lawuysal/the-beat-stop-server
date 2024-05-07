1466bac8fe263aafa75fbf0bc974911ca4b980d0
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-05-07 16:08:14 +0300

    Added "cors" middleware.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const beatRouter = require("./routes/beatRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -15,8 +15,8 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cors());
 app.use(express.json());
-//app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/beats", beatRouter);
 app.use("/api/v1/users", userRouter);
