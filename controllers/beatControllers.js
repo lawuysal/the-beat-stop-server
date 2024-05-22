676b31d4465fffc25071a4421de83941fd1ce7ea
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-05-22 22:24:44 +0300

    Add functionality to beat controllers.

--- controllers/beatControllers.js
+++ controllers/beatControllers.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
+const Beat = require("../models/beatModel");
 
 async function getAllBeats(req, res) {
   try {
+    const beats = await Beat.find();
+
     res.status(200).json({
       status: "success",
+      length: beats.length,
       message: "getAllBeats requested!",
+      data: {
+        beats,
+      },
     });
   } catch (err) {
     res.status(404).json({ status: "fail", message: err });
@@ -13,9 +20,25 @@ async function getAllBeats(req, res) {
 
 async function createBeat(req, res) {
   try {
+    const newBeat = {
+      name: req.body.name,
+      summary: req.body.summary,
+      description: req.body.description,
+      type: req.body.type,
+      license: req.body.license,
+      owner: req.body.owner,
+      bpm: req.body.bpm,
+      key: req.body.key,
+    };
+
+    const beat = await Beat.create(newBeat);
+
     res.status(201).json({
       status: "success",
       message: "createBeat requested!",
+      data: {
+        beat,
+      },
     });
   } catch (err) {
     res.status(400).json({ status: "fail", message: err });
@@ -58,10 +81,29 @@ async function deleteBeat(req, res) {
   }
 }
 
+const getUserBeats = async (req, res) => {
+  try {
+    const beats = await Beat.find({ owner: { $eq: req.params.userId } });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        beats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Can't found the beats for this user." + err,
+    });
+  }
+};
+
 module.exports = {
   getAllBeats,
   getBeat,
   createBeat,
   updateBeat,
   deleteBeat,
+  getUserBeats,
 };
